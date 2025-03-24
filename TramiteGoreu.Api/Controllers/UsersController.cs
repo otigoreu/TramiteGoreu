687c954c1b3ac786457a9b36fe1df96dd514e93f
otigoreu <eponce@regionucayali.gob.pe>
2025-03-24 09:21:08 -0500

    acceso remoto

--- TramiteGoreu.Api/Controllers/UsersController.cs
+++ TramiteGoreu.Api/Controllers/UsersController.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
-using System.Transactions;
 
 namespace Goreu.Tramite.Api.Controllers
 {
